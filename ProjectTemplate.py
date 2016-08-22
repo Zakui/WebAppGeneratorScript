b2c094443cece50b84f3a5598b84aded374cf9ca
Jules Thea <zakuijules@live.fr>
2016-08-22 06:55:44 +0000

    added the readme file

--- ProjectTemplate.py
+++ ProjectTemplate.py
@@ -29,7 +29,7 @@ def jumbotron_content():
         with tag('div',klass="container"):
             with tag('h1'):
                 text('Welcome to Ehealth Academy')
-            texte='''This is a bootstrap Template. It includes a navbar with links and a large jumbotron element. Use it as a starting point to create something more unique.'''
+            texte='''This is a bootstrap and Angular Template. It includes a navbar with links and a large jumbotron element. Use it as a starting point to create your web app.'''
             with tag('p'):
                 text(texte)
             with tag('p'):
@@ -46,30 +46,38 @@ with tag('html'):
     with tag('head'):
         with tag('title'):
             text('Web site Template')
-        doc.stag('link',rel="stylesheet",href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")
-        doc.stag('link',rel="stylesheet",href="/css/style.css")
+        doc.stag('link',rel="stylesheet",href="bower_components/bootstrap/dist/css/bootstrap.min.css")
+        doc.stag('link',rel="stylesheet",href="css/style.css")
     with tag('body'):
         #called the header and jumbotron function
         site_header()
         jumbotron_content()
 
-        with tag('script', type="text/javascript",src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"):
+        with tag('script', type="text/javascript",src="bower_components/angular/angular.min.js"):
             text('')
-        with tag('script', type="text/javascript",src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"):
+        with tag('script', type="text/javascript",src="bower_components/jquery/dist/jquery.min.js"):
+            text('')
+        with tag('script', type="text/javascript",src="bower_components/bootstrap/dist/js/bootstrap.min.js"):
             text('')
 result = indent(doc.getvalue())
 print result
 
 
 #creating css directory and css file
-os.makedirs(os.getcwd()+"\myProject\css")
-os.chdir(os.getcwd()+"\myProject\css")
+try:
+    os.makedirs(os.getcwd()+"\css")
+except Exception as e:
+    pass
+os.chdir(os.getcwd()+"\css")
 custom_css=open('style.css','wb')
 #go back in myProject folder
 os.chdir('..')
 
 #creating js directoryand js file
-os.makedirs(os.getcwd()+"\js")
+try:
+    os.makedirs(os.getcwd()+"\js")
+except Exception as e:
+    pass
 os.chdir(os.getcwd()+"\js")
 custom_js=open('main.css','wb')
 #go back in myProject folder
---
+++ README.md
@@ -0,0 +1,16 @@
+This is a script using to generate a web app project structure, Its includes bootstrap , jquery and angular ready to Use
+
+<h1>Installation</h1>
+Before using this script you must have python installed and bower on your computer and execute these command
+
+
+<h2>Execute this command to install Python yattag module</h2>
+<pre>
+<code>pip Install yattag</code>
+</pre>
+
+
+<h2>Execute this command to install bower devDependencies modules</h2>
+<pre>
+<code>bower Install </code>
+</pre>
